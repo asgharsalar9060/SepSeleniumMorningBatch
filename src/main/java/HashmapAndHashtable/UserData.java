88a8a693d5923ce791837a1431112810450a679c
Asghar <salar2015151@gmail.com>
2021-06-12 10:45:06 +0800

    HashMap added

---
+++ src/main/java/HashmapAndHashtable/UserData.java
@@ -0,0 +1,35 @@
+package HashmapAndHashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UserData {
+
+	public static void main(String[] args) {
+
+		//HashMap<String, String> userMap1 = new HashMap<String, String>();
+		
+		Map<String, String> userMap = new HashMap<String, String>();
+		
+		userMap.put("admin", "admin@gmail.com_admin123");
+		userMap.put("marketer", "marketer@gmail.com_marketer123");
+		userMap.put("manager", "manager@gmail.com_manager123");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.get("https://classic.freecrm.com/index.html");
+		
+		driver.findElement(By.name("username")).sendKeys(userMap.get("marketer").split("_")[0]);
+		driver.findElement(By.name("password")).sendKeys(userMap.get("marketer").split("_")[0]);
+	}
+
+}
